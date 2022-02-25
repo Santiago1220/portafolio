60476353ddc1c6f3822ac2662af978d539902701
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-02-24 21:50:32 -0500

    Se realiza validación del textarea al ingresar caracteres.

--- portafolio/js/formulario.js
+++ portafolio/js/formulario.js
@@ -1,14 +1,17 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
+const textareas = document.querySelectorAll('#formulario textarea');
 
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	mensaje: /^[a-zA-ZÀ-ÿ\S]{1,300}$/
 }
 
 const campos = {
 	nombre: false,
-	correo: false
+	correo: false,
+	mensaje: false
 }
 
 const validarFormulario = (e) =>{
@@ -18,6 +21,9 @@ const validarFormulario = (e) =>{
 		break;
 		case "correo":
             validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "mensaje":
+            validarTextArea(expresiones.mensaje, e.target, 'mensaje');
 		break;
 	}
 }
@@ -40,9 +46,32 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
+const validarTextArea = (expresion, textarea, campo) => {
+	if(expresion.test(textarea.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__validacion-error`).classList.remove('formulario__validacion-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__validacion-error`).classList.add('formulario__validacion-error-activo');
+		campos[campo] = false;
+	}
+}
+
 inputs.forEach((input)=>{
     input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
+});
+
+textareas.forEach((textarea)=>{
+    textarea.addEventListener('keyup', validarFormulario);
+	textarea.addEventListener('blur', validarFormulario);
 });
 
 formulario.addEventListener('submit', (e)=>{
