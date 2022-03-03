fa4af4a52410dd32665f0a44d49639917cfa0a5a
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-03-02 21:58:34 -0500

    Se realiza validación final del formulario para poder recibir con el botón cuando el formulario esta completo o incompleto y muestre los mensajes correspondientes

--- portafolio/js/formulario.js
+++ portafolio/js/formulario.js
@@ -76,5 +76,23 @@ textareas.forEach((textarea)=>{
 
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.nombre && campos.correo && campos.mensaje && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+		document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
 });
 
